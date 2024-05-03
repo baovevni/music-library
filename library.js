a13ce1cf678836437653a849ac03fc36c224cf2a
Evgheni Barducov <52833790+baovevni@users.noreply.github.com>
2024-05-03 16:52:14 -0400

    implemented library.js

--- library.js
+++ library.js
@@ -1,26 +1,36 @@
 const library = {
-  tracks: { t01: { id: "t01",
-                   name: "Code Monkey",
-                   artist: "Jonathan Coulton",
-                   album: "Thing a Week Three" },
-            t02: { id: "t02",
-                   name: "Model View Controller",
-                   artist: "James Dempsey",
-                   album: "WWDC 2003"},
-            t03: { id: "t03",
-                   name: "Four Thirty-Three",
-                   artist: "John Cage",
-                   album: "Woodstock 1952"}
-          },
-  playlists: { p01: { id: "p01",
-                      name: "Coding Music",
-                      tracks: ["t01", "t02"]
-                    },
-               p02: { id: "p02",
-                      name: "Other Playlist",
-                      tracks: ["t03"]
-                    }
-             }
+       tracks: {
+              t01: {
+                     id: "t01",
+                     name: "Code Monkey",
+                     artist: "Jonathan Coulton",
+                     album: "Thing a Week Three"
+              },
+              t02: {
+                     id: "t02",
+                     name: "Model View Controller",
+                     artist: "James Dempsey",
+                     album: "WWDC 2003"
+              },
+              t03: {
+                     id: "t03",
+                     name: "Four Thirty-Three",
+                     artist: "John Cage",
+                     album: "Woodstock 1952"
+              }
+       },
+       playlists: {
+              p01: {
+                     id: "p01",
+                     name: "Coding Music",
+                     tracks: ["t01", "t02"]
+              },
+              p02: {
+                     id: "p02",
+                     name: "Other Playlist",
+                     tracks: ["t03"]
+              }
+       }
 };
 
 /////////////////////////////
@@ -31,8 +41,13 @@ const library = {
 // p01: Coding Music - 2 tracks
 // p02: Other Playlist - 1 tracks
 const printPlaylists = function() {
-
+       for (const playlistId in library.playlists) {
+              const playlistInfo = library.playlists[playlistId];
+              console.log(`${playlistInfo.id}: ${playlistInfo.name} - ${playlistInfo.tracks.length} tracks`);
+       }
 }
+
+// printPlaylists();
 
 
 // prints a list of all tracks, using the following format:
@@ -40,8 +55,12 @@ const printPlaylists = function() {
 // t02: Model View Controller by James Dempsey (WWDC 2003)
 // t03: Four Thirty-Three by John Cage (Woodstock 1952)
 const printTracks = function() {
-
+       for (const trackId in library.tracks) {
+              const trackInfo = library.tracks[trackId];
+              console.log(`${trackId}: ${trackInfo.name} by ${trackInfo.artist} (${trackInfo.album})`)
+       }
 }
+// printTracks();
 
 
 // prints a list of tracks for a given playlist, using the following format:
@@ -49,33 +68,57 @@ const printTracks = function() {
 // t01: Code Monkey by Jonathan Coulton (Thing a Week Three)
 // t02: Model View Controller by James Dempsey (WWDC 2003)
 const printPlaylist = function(playlistId) {
+       const playlistInfo = library.playlists[playlistId];
+       console.log(`${playlistInfo.id}: ${playlistInfo.name} - ${playlistInfo.tracks.length} tracks`);
+       for (const trackId of playlistInfo.tracks) {
+              const trackInfo = library.tracks[trackId];
+              console.log(`${trackId}: ${trackInfo.name} by ${trackInfo.artist} (${trackInfo.album})`);
+       }
 
 }
+// printPlaylist("p01");
 
 
 // adds an existing track to an existing playlist
 const addTrackToPlaylist = function(trackId, playlistId) {
-
+       library.playlists[playlistId].tracks.push(trackId);
 }
-
+// addTrackToPlaylist("t01", "p02");
+// console.log(library.playlists);
 
 // generates a unique id
 // (already implemented: use this for addTrack and addPlaylist)
 const generateUid = function() {
-  return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+       return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
 }
 
 
 // adds a track to the library
 const addTrack = function(name, artist, album) {
-
+       const key = generateUid();
+       const value = {
+              id: key,
+              name: name,
+              artist: artist,
+              album: album
+       };
+       library.tracks[key] = value;
 }
+// addTrack("John", "Travolta", "Sunrise 2020");
+// console.log(library.tracks);
 
 
 // adds a playlist to the library
 const addPlaylist = function(name) {
-
+       const key = generateUid();
+       const value = {
+              id: key,
+              name: name,
+              tracks: []
+       };
+       library.playlists[key] = value;
 }
+// addPlaylist("Playlist 123");
 
 
 // STRETCH:
